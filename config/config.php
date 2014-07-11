e8624e922cd24bdbee3e028a3c0441bbb7b16112
leonardofln <leonardofln@gmail.com>
2014-07-11 00:18:40 -0300

    adicionado o atributo theme com o valor gtcnc para reconhecer o tema personalizado do GT

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oca6c1302339',
+  'passwordsalt' => 'c85e4d66686c5c9ef127c044ed8dd1',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => 'C:\\xampp\\htdocs\\owncloud-6.0.4\\data',
+  'dbtype' => 'mysql',
+  'version' => '6.0.4.1',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'b3a97034ce6d44e80f47cb9e422fe7',
+  'installed' => true,
+  'theme' => 'gtcnc',
+);
